0ecbe9a76eb56c188d7edec51be07042aa022297
Rohulamin <rohulaminwd@gmail.com>
2021-06-08 10:56:54 +0600

    fast commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+new WOW().init();
+
+
+var typed = new Typed('.type', {
+    strings: [
+        'Rohul amin',
+        'Rohul amin', 
+        'web desainer!',],
+        typeSpeed: 100,
+        loop: true,
+      
+  });
+  var typed = new Typed('.typee', {
+    strings: [
+        'Rohul amin',
+        'Rohul amin'],
+        typeSpeed: 100,
+        loop: true,
+      
+  });
